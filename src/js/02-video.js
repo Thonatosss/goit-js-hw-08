c42557f1f2bdc1c6d9df60ba5caeb6e9d9623a2b
Mykola Romashenko <sven4369@gmail.com>
2023-02-23 15:03:37 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const LOCAL_STORAGE_KEY = "videoplayer-current-time"
+const player = new Player(iframe, options);
+
+const options = {
+    controls: false,
+
+}
+player.on('timeupdate', throttle(function (time) {
+    const Playertime = {
+        playerCurrentTime: time.seconds,
+    }
+    function updateStorage() {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(Playertime));
+    }
+    updateStorage();
+}, 1000));
+const savedSettings = localStorage.getItem(LOCAL_STORAGE_KEY);
+const parsedSettings = JSON.parse(savedSettings);
+
+player.setCurrentTime(parsedSettings.playerCurrentTime);
+
+
 
