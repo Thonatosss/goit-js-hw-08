ea9fce48082532af466121dd8aa3e36eee31064a
Mykola Romashenko <sven4369@gmail.com>
2023-02-26 20:28:10 +0300

    first task fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,18 +6,9 @@ const LOCAL_STORAGE_KEY = "videoplayer-current-time"
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(function (time) {
-    const Playertime = {
-        playerCurrentTime: time.seconds,
-    }
-    function updateStorage() {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(Playertime));
-    }
-    updateStorage();
+    localStorage.setItem(LOCAL_STORAGE_KEY, time.seconds);
 }, 1000));
-const savedSettings = localStorage.getItem(LOCAL_STORAGE_KEY);
-const parsedSettings = JSON.parse(savedSettings);
-
-player.setCurrentTime(parsedSettings.playerCurrentTime);
+player.setCurrentTime(localStorage.getItem(LOCAL_STORAGE_KEY));
 
 
 
